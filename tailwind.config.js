bf5347f13c73e6346010afb86085806e91d9ddbf
harshilvagadiya <harshilvagadiya786@gmail.com>
2025-03-23 15:34:57 +0530

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1e293b",
+        secondary: "#14b8a6",
+        bgLight: "#f8fafc",
+        bgDark: "#0f172a",
+        highlight: "#facc15",
+        textDark: "#334155",
+      },
+    },
+  },
+  plugins: [],
+};
